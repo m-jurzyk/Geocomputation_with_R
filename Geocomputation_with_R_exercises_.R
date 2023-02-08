9e888d2642f784cf83d7d0ac3d085494b357f5a2
m-jurzyk <jurzyk.maciej@gmail.com>
2023-02-08 13:53:29 +0100

    Book organizational file

---
+++ Geocomputation_with_R_exercises_.R
@@ -0,0 +1,21 @@
+
+#Geocomputation with R----
+
+#Robin Lovelace, Jakub Nowosad, Jannes Muenchow
+
+#2021-08-27
+
+#To reproduce the code in the book, you need a recent version of R and up-to-date packages. 
+#These can be installed with the following command (which requires remotes):
+
+#remotes::install_github("geocompr/geocompkg")
+
+#To build the book locally, clone or download the geocompr repo, load R in root directory
+#(e.g. by opening geocompr.Rproj in RStudio) and run the following lines:
+
+bookdown::render_book("index.Rmd") # to build the book
+
+browseURL("_book/index.html") # to view i
+
+##Chapter 1 Introduction ---- 
+
