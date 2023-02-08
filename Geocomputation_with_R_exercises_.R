1664f5d12e2da10de5c4c0bd6a10f730ec4d596f
m-jurzyk <jurzyk.maciej@gmail.com>
2023-02-08 20:45:42 +0100

    Chapter 2, exercises chapter 2: 1-2

--- Geocomputation_with_R_exercises_.R
+++ Geocomputation_with_R_exercises_.R
@@ -75,7 +75,7 @@ library(raster)
 library(spData)
 library(spDataLarge)
 
-##2.2.1 An introduction to simple features ---- 
+###2.2.1 An introduction to simple features ---- 
 
 
 names(world)
@@ -84,13 +84,13 @@ plot(world)
 
 summary(world["lifeExp"])
 
-##2.2.2 Why simple features? ---- 
+###2.2.2 Why simple features? ---- 
 
 library(sp)
 
 world_sp = as(world, Class = "Spatial")
 
-##2.2.3 Basic map making ----
+###2.2.3 Basic map making ----
 
 plot(world[5:6])
 plot(world["pop"])
@@ -102,7 +102,7 @@ plot(world["pop"], reset = FALSE)
 plot(asia, add = TRUE, col = "red")
 
 
-##2.2.4 Base plot arguments ---- 
+###2.2.4 Base plot arguments ---- 
 
 
 plot(world["continent"], reset = FALSE)
@@ -114,3 +114,106 @@ plot(st_geometry(world_cents), add = TRUE, cex = cex)
 india = world[world$name_long == "India", ]
 plot(st_geometry(india), expandBB = c(0, 0.2, 0.1, 1), col = "gray", lwd = 3)
 plot(world_asia[0], add = TRUE)
+
+
+###2.2.5 Geometry types - skip
+
+###2.2.6 Simple feature geometries (sfg) ---- 
+
+#A point: st_point()
+#A linestring: st_linestring()
+#A polygon: st_polygon()
+#A multipoint: st_multipoint()
+#A multilinestring: st_multilinestring()
+#A multipolygon: st_multipolygon()
+#A geometry collection: st_geometrycollection()
+
+
+## 2.3 Raster data ---- 
+
+
+raster_filepath = system.file("raster/srtm.tif", package = "spDataLarge")
+new_raster = raster(raster_filepath)
+
+new_raster
+
+plot(new_raster)
+
+spplot(new_raster)
+
+levelplot(new_raster)
+
+raster_filepath = system.file("raster/srtm.tif", package = "spDataLarge")
+new_raster = raster(raster_filepath)
+
+new_raster2 = raster(nrows = 6, ncols = 6, res = 0.5, 
+                     xmn = -1.5, xmx = 1.5, ymn = -1.5, ymx = 1.5,
+                     vals = 1:36)
+
+## 2.4 Coordinate Reference Systems ---- 
+
+library(rgdal)
+
+crs_data = rgdal::make_EPSG()
+View(crs_data)
+
+
+vector_filepath = system.file("vector/zion.gpkg", package = "spDataLarge")
+new_vector = st_read(vector_filepath)
+
+st_crs(new_vector) # get CRS
+
+new_vector = st_set_crs(new_vector, 4326) # set CRS
+
+###2.6 Exercises ---- 
+
+#1_ Use summary() on the geometry column of the world data object. 
+# What does the output tell us about:
+
+summary(world)
+
+  #1_a Its geometry type?
+
+# Multipolygon 
+
+  #1_b The number of countries?
+
+# 177?
+
+  #1_3 Its coordinate reference system (CRS)?
+
+# epsg:4326
+
+#2_ Run the code that ‘generated’ the map of the world in Figure 2.5 
+#at the end of Section 2.2.4. 
+
+plot(world["continent"], reset = FALSE)
+cex = sqrt(world$pop) / 10000
+world_cents = st_centroid(world, of_largest = TRUE)
+plot(st_geometry(world_cents), add = TRUE, cex = cex)
+
+
+#Find two similarities and two differences between the image on your computer 
+#and that in the book.
+
+
+# Differences: 
+# Line weight of circles? 
+# Projection system?
+# Earth grid 
+# Scale 
+
+# Similarities:
+
+# Color palette
+# Data are probably the same 
+
+
+#2_a What does the cex argument do (see ?plot)?
+#2_b Why was cex set to the sqrt(world$pop) / 10000?
+#2_c Bonus: experiment with different ways to visualize the global population.
+
+
+  
+
+
