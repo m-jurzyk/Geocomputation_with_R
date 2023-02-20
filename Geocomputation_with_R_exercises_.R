610f4a9cf8a70275c498cb588ae797ddde23b53f
m-jurzyk <jurzyk.maciej@gmail.com>
2023-02-20 12:02:44 +0100

    Chapter 4.2.4 to 4.4

--- Geocomputation_with_R_exercises_.R
+++ Geocomputation_with_R_exercises_.R
@@ -824,4 +824,165 @@ plot(random_joined)
 
 
 
+#### 4.2.4 Non-overlapping joins ----
+
+plot(st_geometry(cycle_hire), col = "blue")
+plot(st_geometry(cycle_hire_osm), add = TRUE, pch = 3, col = "red")
+
+# Distance 20 and projection change 
+
+cycle_hire_P = st_transform(cycle_hire, 27700)
+cycle_hire_osm_P = st_transform(cycle_hire_osm, 27700)
+sel = st_is_within_distance(cycle_hire_P, cycle_hire_osm_P, dist = 20)
+summary(lengths(sel) > 0)
+
+
+# FALSE 304     TRUE  438 #This shows that there are 438 points in the
+#target object cycle_hire_P within the threshold distance of cycle_hire_osm_P
+# distance argument set to below 20m 
+
+z = st_join(cycle_hire_P, cycle_hire_osm_P,
+            join = st_is_within_distance, dist = 20)
+
+nrow(cycle_hire)
+
+nrow(z)
+
+z = z %>% 
+  group_by(id) %>% 
+  summarize(capacity = mean(capacity))
+nrow(z) == nrow(cycle_hire)
+
+plot(cycle_hire_osm["capacity"])
+plot(z["capacity"])
+
+
+#### 4.2.5 Spatial data aggregation ----
+
+nz_avheight = aggregate(x = nz_height, by = nz, FUN = mean)
+
+nz_avheight2 = nz %>%
+  st_join(nz_height) %>%
+  group_by(Name) %>%
+  summarize(elevation = mean(elevation, na.rm = TRUE))
+
+agg_aw = st_interpolate_aw(incongruent[, "value"], aggregating_zones,
+                           extensive = TRUE)
+#> Warning in st_interpolate_aw.sf(incongruent[, "value"], aggregating_zones, :
+#> st_interpolate_aw assumes attributes are constant or uniform over areas of x
+# show the aggregated result
+agg_aw$value
+
+#### 4.2.6 Distance relations----
+
+nz_heighest = nz_height %>% top_n(n = 1, wt = elevation)
+canterbury_centroid = st_centroid(canterbury)
+st_distance(nz_heighest, canterbury_centroid)
+
+# it is returned as a matrix in meters unit 
+
+co = filter(nz, grepl("Canter|Otag", Name))
+st_distance(nz_height[1:3, ], co)
+
+plot(st_geometry(co)[2])
+plot(st_geometry(nz_height)[2:3], add = TRUE)
+
+
+##4.3 Spatial operations on raster data----
+
+# elev and grain can be found in spData package
+
+#### 4.3.1 Spatial subsetting----
+
+id = cellFromXY(elev, xy = c(0.1, 0.1))
+elev[id]
+# the same as
+raster::extract(elev, data.frame(x = 0.1, y = 0.1))
+
+#clip 
+
+clip = raster(xmn = 0.9, xmx = 1.8, ymn = -0.45, ymx = 0.45,
+              res = 0.3, vals = rep(1, 9))
+elev[clip]
+
+elev[1:2, drop = FALSE]    # spatial subsetting with cell IDs
+elev[1, 1:2, drop = FALSE] # spatial subsetting by row,column indices
+
+
+
+# create raster mask
+rmask = elev 
+values(rmask) = sample(c(NA, TRUE), 36, replace = TRUE)
+
+# spatial subsetting
+elev[rmask, drop = FALSE]           # with [ operator
+mask(elev, rmask)                   # with mask()
+overlay(elev, rmask, fun = "max")   # with overlay
+
+#### 4.3.2 Map algebra----
+
+# Theory about map algebra 
+
+#### 4.3.3 Local operations ----
+
+
+rcl = matrix(c(0, 12, 1, 12, 24, 2, 24, 36, 3), ncol = 3, byrow = TRUE)
+recl = reclassify(elev, rcl = rcl)
+
+# Included in RASTER package 
+
+elev + elev
+elev^2
+log(elev)
+elev > 5
+
+## calc() - overlay() functions in raster 
+
+# NDVI calculations # Satellite data raster calculations 
+
+
+# B4 path "/Users/maciejjurzyk/Downloads/S2B_MSIL1C_20230205T111139_N0509_R137_T29SQB_20230205T115207.SAFE/GRANULE/L1C_T29SQB_A030908_20230205T111142/IMG_DATA/T29SQB_20230205T111139_B04.jp2"
+
+# B8 path "/Users/maciejjurzyk/Downloads/S2B_MSIL1C_20230205T111139_N0509_R137_T29SQB_20230205T115207.SAFE/GRANULE/L1C_T29SQB_A030908_20230205T111142/IMG_DATA/T29SQB_20230205T111139_B08.jp2"
+
+
+red <- rast("/Users/maciejjurzyk/Downloads/S2B_MSIL1C_20230205T111139_N0509_R137_T29SQB_20230205T115207.SAFE/GRANULE/L1C_T29SQB_A030908_20230205T111142/IMG_DATA/T29SQB_20230205T111139_B04.jp2")
+
+nir <- rast("/Users/maciejjurzyk/Downloads/S2B_MSIL1C_20230205T111139_N0509_R137_T29SQB_20230205T115207.SAFE/GRANULE/L1C_T29SQB_A030908_20230205T111142/IMG_DATA/T29SQB_20230205T111139_B08.jp2")
+
+ndvi = (nir - red) / (nir + red)
+
+plot(ndvi)
+
+#### 4.3.4 Focal operations ----
+
+# focal function to spatial filetering 
+
+r_focal = focal(elev, w = matrix(1, nrow = 3, ncol = 3), fun = min)
+
+
+#### 4.3.5 Zonal operations ----
+
+z = zonal(elev, grain, fun = "mean") %>%
+  as.data.frame()
+
+z
+
+
+#### 4.3.6 Global operations and distances ----
+
+#### 4.3.7 Map algebra counterparts in vector processing ----
+
+#### 4.3.8 Merging rasters ----
+
+aut = getData("alt", country = "AUT", mask = TRUE)
+ch = getData("alt", country = "CHE", mask = TRUE)
+aut_ch = merge(aut, ch)
+
+### 4.4 Exercises  ====
+
+#It was established in Section 4.2 that Canterbury was the region of New Zealand 
+#containing most of the 100 highest points in the country.
+#How many of these high points does the Canterbury region contain?
+
 
